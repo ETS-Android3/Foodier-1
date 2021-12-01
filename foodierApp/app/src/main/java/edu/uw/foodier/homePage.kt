1de4e95859ed081cf946d4127f31cf52a031bb1a
Jade D'Souza <jadedsou@uw.edu>
2021-11-30 23:21:08 -0800

    got navigation to work

--- foodierApp/app/src/main/java/edu/uw/foodier/homePage.kt
+++ foodierApp/app/src/main/java/edu/uw/foodier/homePage.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import edu.uw.foodier.databinding.HomePageBinding
-import edu.uw.foodier.databinding.HomePageBindingImpl
 
 class homePage : Fragment() {
     private var _binding: HomePageBinding? = null
