194b8a9058bc51afe3f48eb994f684c611382966
Jade D'Souza <jadedsou@uw.edu>
2021-12-02 14:11:44 -0800

    fixed nav into bookmarks

--- foodierApp/app/src/main/java/edu/uw/foodier/MainActivity.kt
+++ foodierApp/app/src/main/java/edu/uw/foodier/MainActivity.kt
@@ -6,18 +6,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import edu.uw.foodier.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    //private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(binding.root)
 
-        binding.button.setOnClickListener { view ->
+        button.setOnClickListener { view ->
             // goes to second activity
             val goToSecondActivity = Intent(this, BookmarkActivity::class.java)
 
