388babc1807c6b62a261490ba744fa1c310640fc
Jade D'Souza <jadedsou@uw.edu>
2021-12-02 14:20:21 -0800

    food class done

---
+++ foodierApp/app/src/main/java/edu/uw/foodier/FoodItem.kt
@@ -0,0 +1,8 @@
+package edu.uw.foodier
+//could be @parcelize
+class FoodItem (
+    val food_name : String,
+    val food_image: String,
+    val restaurant: String,
+    val address : String
+) // : Parcelable
--- foodierApp/app/src/main/java/edu/uw/foodier/MainActivity.kt
+++ foodierApp/app/src/main/java/edu/uw/foodier/MainActivity.kt
@@ -5,18 +5,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import edu.uw.foodier.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    //private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //binding = ActivityMainBinding.inflate(layoutInflater)
-        //setContentView(binding.root)
 
         button.setOnClickListener { view ->
             // goes to second activity
