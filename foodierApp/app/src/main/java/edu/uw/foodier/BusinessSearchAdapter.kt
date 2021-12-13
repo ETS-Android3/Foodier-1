bf3ba02b4d0878799dad46389b768eb017ef6eca
Jade D'Souza <jadedsou@uw.edu>
2021-12-12 17:44:37 -0800

    almost done

---
+++ foodierApp/app/src/main/java/edu/uw/foodier/BusinessSearchAdapter.kt
@@ -0,0 +1,37 @@
+package edu.uw.foodier
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import edu.uw.foodier.model.Business
+
+class BusinessSearchAdapter(
+    val businesses: List<Business>
+) : RecyclerView.Adapter<BusinessSearchAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(parent)
+    override fun getItemCount() = businesses.size
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(businesses[position])
+    }
+
+    class ViewHolder(
+        parent: ViewGroup
+    ) : RecyclerView.ViewHolder(parent.inflate(R.layout.business_search_result)) {
+        val image: ImageView = itemView.findViewById(R.id.imageBus)
+        val name: TextView = itemView.findViewById(R.id.name)
+
+        fun bind(business: Business) {
+            name.text = business.name
+            Glide.with(image.context).load(business.imageUrl).into(image)
+        }
+    }
+}
+
+private fun ViewGroup.inflate(@LayoutRes layout: Int): View {
+    return LayoutInflater.from(context).inflate(layout, this, false)
+}
