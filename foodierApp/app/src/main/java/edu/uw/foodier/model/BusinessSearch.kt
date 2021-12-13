12c30784c6829bb75bded2237b824caf3ee54193
Jade D'Souza <jadedsou@uw.edu>
2021-12-12 17:20:51 -0800

    getting it to work for washington

--- foodierApp/app/src/main/java/edu/uw/foodier/model/BusinessSearch.kt
+++ foodierApp/app/src/main/java/edu/uw/foodier/model/BusinessSearch.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface BusinessSearch {
-    @GET("v3/businesses/search?location=SF")
+    @GET("v3/businesses/search?location=WA")
     fun search(@Query("term") term: String): Single<BusinessSearchResponse>
 }
